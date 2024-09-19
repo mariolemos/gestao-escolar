2372ac7852855d97ff5555fee20781e3b43d0031
lemos <lemos>
2024-09-19 09:29:23 -0300

    Projeto Criado

---
+++ src/test/java/br/com/mariolemos/gestaoEscolar/GestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mariolemos.gestaoEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
